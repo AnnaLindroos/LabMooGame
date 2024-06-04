c01a4e6e84e42a2208d1ae468dfff1d18f607f5d
AnnaLindroos <alindroos98@gmail.com>
2024-06-04 15:09:40 +0200

    Temporary comments for clarification

--- Program.cs
+++ Program.cs
@@ -33,7 +33,6 @@ class MainClass
             //Renamed to correctAnswer instead of makeGoal (small letter)
             string correctAnswer = WinGame();
 
-
             Console.WriteLine("New game:\n");
             //comment out or remove next line to play real games!
             //Console.WriteLine("For practice, number is: " + correctAnswer + "\n");
@@ -41,9 +40,9 @@ class MainClass
             //Renamed to userGuess for clarity
             string userGuess = Console.ReadLine();
 
-            //Renamed from nGuess for clarity
+            //Renamed to numberOfGuesses from nGuess for clarity
             int numberOfGuesses = 1;
-            //Renamed from bbcc for clarity
+            //Renamed to bullsAndCows from bbcc for clarity
             string bullsAndCows = CheckBullsAndCows(correctAnswer, userGuess);
             Console.WriteLine(bullsAndCows + "\n");
             while (bullsAndCows != "BBBB,")
@@ -57,10 +56,10 @@ class MainClass
             StreamWriter output = new StreamWriter("result.txt", append: true);
             output.WriteLine(userName + "#&#" + numberOfGuesses);
             output.Close();
-            //Renamed from showTopList for clarity
+            //Renamed to HighScore from showTopList for clarity
             HighScore();
             Console.WriteLine("Correct, it took " + numberOfGuesses + " guesses\nContinue?");
-            //renamed from answer for clarity
+            //renamed to keepPlaying from answer for clarity
             string keepPlaying = Console.ReadLine();
             if (keepPlaying != null && keepPlaying != "" && keepPlaying.Substring(0, 1) == "n")
             {
@@ -70,29 +69,29 @@ class MainClass
     }
     static string WinGame()
     {
-        //Renamed from randomGenerator
+        //Renamed to randomNumbers from randomGenerator
         Random randomNumbers = new Random();
-        //Renamed from goal
+        //Renamed to correctAnswer from goal
         string correctAnswer = "";
         // Magic number, change to a constant int called max.
         for (int i = 0; i < MAX; i++)
         {
-            //Changed from random
+            //Changed to randomNumber from random
             int randomNumber = randomNumbers.Next(10);
-            //Changedfrom randomDigit
+            //Changed to generatedNumberSequence from randomDigit
             string generatedNumberSequence = "" + randomNumber;
             while (correctAnswer.Contains(generatedNumberSequence))
             {
                 randomNumber = randomNumbers.Next(10);
                 generatedNumberSequence = "" + randomNumber;
             }
-            // shortened from correctAnswer = correctAnswer + generatedNumberSequence;
+            // shortened to correctAnswer += generatedNumberSequence from correctAnswer = correctAnswer + generatedNumberSequence;
             correctAnswer += generatedNumberSequence;
         }
         return correctAnswer;
     }
 
-    //Renamed from checkBC (small letter at the start) for clarity
+    //Renamed to CheckBullsAndCows from checkBC (small letter at the start) for clarity
     static string CheckBullsAndCows(string goal, string guess)
     {
         int cows = 0, bulls = 0;
@@ -117,7 +116,7 @@ class MainClass
         return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
     }
 
-    //Renamed from showTopList (small letter) for clarity
+    //Renamed to HighScore from showTopList (small letter) for clarity
     static void HighScore()
     {
         StreamReader input = new StreamReader("result.txt");
@@ -128,7 +127,7 @@ class MainClass
             string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
             string name = nameAndScore[0];
             int guesses = Convert.ToInt32(nameAndScore[1]);
-            //Renamed from pd for clarity
+            //Renamed to playerData from pd for clarity
             PlayerData playerData = new PlayerData(name, guesses);
             int pos = results.IndexOf(playerData);
             if (pos < 0)
@@ -146,7 +145,7 @@ class MainClass
         }
         results.Sort((p1, p2) => p1.AverageGuesses().CompareTo(p2.AverageGuesses()));
         Console.WriteLine("Player   games average");
-        //Renamed from p to player for clarity
+        //Renamed to player from p to player for clarity
         foreach (PlayerData player in results)
         {
             Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", player.PlayerName, player.NumberOfGames, player.AverageGuesses()));
@@ -157,16 +156,16 @@ class MainClass
 
 class PlayerData
 {
-    //Renamed from PlayerName for clarity
+    //Renamed to PlayerName from Name for clarity
     public string PlayerName { get; private set; }
-    //Renamed from NGames
+    //Renamed to NumberOfGames from NGames
     public int NumberOfGames { get; private set; }
 
-    //changed this to private for clarity. Renamed from totalGuess (small letter)
+    //changed this to private for clarity. Renamed to GuessesInTotal from totalGuess (small letter)
     private int GuessesInTotal;
 
 
-    // renamed from name 
+    // renamed to playerName from name 
     public PlayerData(string playerName, int guesses)
     {
         this.PlayerName = playerName;
@@ -185,14 +184,14 @@ class PlayerData
         NumberOfGames++;
     }
 
-    // Renamed from AverageGuesses
+    // Renamed to AverageGuesses from Average
     public double AverageGuesses()
     {
         return (double)GuessesInTotal / NumberOfGames;
     }
 
 
-    //Renamed method name from Equals. Renamed object from p. Removed the override (not necessary after name refactoring)
+    //Renamed method name to SamePlayer from Equals. Renamed object to player from p. Removed the override (not necessary after name refactoring)
     public bool SamePlayer(Object player)
     {
         return PlayerName.Equals(((PlayerData)player).PlayerName);
