acad91c4514414103296c9ebe8329ac4cd8cbc0c
AnnaLindroos <92397151+AnnaLindroos@users.noreply.github.com>
2024-08-30 14:31:26 +0200

    Add files via upload
    added test files

---
+++ DataContextTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabMooGame.Data;
+using LabMooGame.Models;
+
+namespace LabMooGameTests1;
+
+[TestClass]
+public class DataContextTests
+{
+    private DataContext _dataContext;
+    private List<Player> _players;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _dataContext = new DataContext(new DataContextConfig("testMooGame", "testMasterMind"));
+        _dataContext.CreateFile("Anna", 2, true);
+        _players = new List<Player>();
+    }
+
+    [TestMethod]
+    public void DataContextTest()
+    {
+        Assert.IsNotNull(_dataContext);
+    }
+
+    [TestMethod]
+    public void GetFilePathTest()
+    {
+        Assert.AreEqual("testMooGame", _dataContext.GetFilePath(true));
+    }
+
+    [TestMethod]
+    public void CheckIfPlayerExistsUpdateTest()
+    {
+        string playerName = "Anna";
+        int guesses = 2;
+
+        _dataContext.CheckIfPlayerExists(_players, "Anna", 2);
+        Assert.AreEqual(1, _players.Count);
+        Assert.AreEqual(playerName, _players[0].PlayerName);
+        Assert.AreEqual(guesses, _players[0].GuessesInTotal);
+    }
+
+    [TestMethod]
+    public void CheckIfPlayerExistsAddTest()
+    {
+        string playerName = "Anna";
+        int guesses = 5;
+        int newGuesses = 2;
+        Player existingPlayer = new Player(playerName, guesses);
+        _players.Add(existingPlayer);
+
+        _dataContext.CheckIfPlayerExists(_players, playerName, newGuesses);
+        Assert.AreEqual(1, _players.Count());
+        Assert.AreEqual(7, _players[0].GuessesInTotal);
+    }
+}
---
+++ PlayerTests.cs
@@ -0,0 +1,54 @@
+﻿using LabMooGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMooGameTests1;
+
+[TestClass]
+public class PlayerTests
+{
+    private string _playerName;
+    private int _guesses;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _playerName = "Anna";
+        _guesses = 5;
+    }
+
+    [TestMethod]
+    public void PlayerContructorTest()
+    {
+        Player player = new Player(_playerName, _guesses);
+
+        Assert.AreEqual(_playerName, player.PlayerName);
+        Assert.AreEqual(1, player.NumberOfGames);
+        Assert.AreEqual(_guesses, player.GuessesInTotal);
+    }
+
+    [TestMethod]
+    public void UpdatePlayerHighScoreTest()
+    {
+        Player player = new Player(_playerName, _guesses);
+
+        player.UpdatePlayerHighScore(3);
+
+        Assert.AreEqual(2, player.NumberOfGames);
+        Assert.AreEqual(8, player.GuessesInTotal);
+    }
+
+    [TestMethod]
+    public void GetAverageGuessesTest()
+    {
+        Player player = new Player(_playerName, 6);
+        player.UpdatePlayerHighScore(4);
+
+        double averageGuesses = player.GetAverageGuesses();
+
+        Assert.AreEqual(5.0, averageGuesses);
+    }
+}
